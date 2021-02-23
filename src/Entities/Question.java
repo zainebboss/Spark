9d22d2a78820fe1a2c7ee2af7b2b39f80c799de9
fatmaboussaid <fatma.boussaid@esprit.tn>
2021-02-23 12:46:21 +0100

    creation des entities

---
+++ src/Entities/Question.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+/**
+ *
+ * @author Zaineb
+ */
+public class Question {
+    int ID;
+    String question;
+    String reponse;
+    Examen examen[];
+
+    public Question() {
+    }
+
+    public Question(int ID, String question, String reponse) {
+        this.ID = ID;
+        this.question = question;
+        this.reponse = reponse;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public Examen[] getExamen() {
+        return examen;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setExamen(Examen[] examen) {
+        this.examen = examen;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" + "ID=" + ID + ", question=" + question + ", reponse=" + reponse + '}';
+    }
+    
+    
+}
